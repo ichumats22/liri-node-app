b94a79e980b99a8abd5ef03ec17c0b1088cf3ab3
ichumats <ivanna.chumatschko22@gmail.com>
2019-12-13 17:54:15 -0700

    adding bonus functionality

--- liri.js
+++ liri.js
@@ -8,7 +8,7 @@ var spotify = new Spotify(keys.spotify);
 var command = process.argv[2]
 
 var input = process.argv.slice(3).join(' ');
-//console.log(input);
+var output = [];
 
 runLiri(command, input);
 
@@ -29,44 +29,88 @@ function runLiri(command, input) {
   }
 };
 
+function addToLog(output) {
+  fs.appendFile("log.txt", output, function(error) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      console.log("Log entry added");
+    }
+  });
+}
+
+
 function runBandsInTown(input) {
   var request = require('request');
   request(`https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`, function (error, response, body) {
     if (error) {
-      console.log('error:', error);
-      console.log('statusCode:', response && response.statusCode);
+      output = `Bands In Town Error: ${error}`, `statusCode: ${response && response.statusCode}`;
+      addToLog(output);
+      console.log(output);
     }
     results = JSON.parse(body);
     if (results.length < 1) {
-      console.log(`We're sorry but it looks like ${input} does not currently have any shows scheduled. Please try another artist.`);
+      output = `We're sorry but it looks like ${input} does not currently have any shows scheduled. Please try another artist.`
+      addToLog(output);
+      console.log(output);
     } else {
+      output.push(`Have fun watching ${input} live!`);
       console.log(`Have fun watching ${input} live!`);
       for (let i = 0; i < 5 && i < results.length; i++) {
-        console.log(
-          `Venue: ${results[i].venue.name}
-          Location: ${results[i].venue.city}, ${results[i].venue.country}`);
+        var venue = results[i].venue.name;
+        var location = `${results[i].venue.city}, ${results[i].venue.country}` 
+
         var eventDate = results[i].datetime.split('T')[0];
         eventDate = moment(eventDate, 'YYYY-MM-DD').format('MMM DD, YYYY');
         if (eventDate) {
-            console.log(`Date: ${eventDate}`);
+          date = eventDate;
         } else {
-          console.log('Date: TBA. Please check again later!')
+          date = 'TBD'
         }
-      }
-    } 
+        var date = eventDate
+        
+
+        var event = `Venue: ${venue} 
+        Location: ${location} 
+        Date: ${date}`
+        output.push(event)
+        console.log(event);
+      } 
+      addToLog(`
+      ${output[0]}
+      ${output[1]}
+      ${output[2]}
+      ${output[3]}
+      ${output[4]}
+      ${output[5]}`);
+    }
   });
 };
 
 function runSpotify(input) {
-  spotify.search({type: 'track', query: input, limit: 1}, function (err, data) {
-    if (err) {
-      return console.log('Error ocurred: ' + err);
+  spotify.search({type: 'track', query: input, limit: 5}, function (error, data) {
+    if (error) {
+      output = `Spotify Search Error Ocurred: ${error}`;
+      addToLog(output);
+      return console.log(output);
     } else {
-      song = data.tracks.items[0];
-      console.log(`Artist: ${song.artists[0].name} 
-      Song: ${song.name}
-      Preview Link: ${song.preview_url}
-      Album: ${song.album.name}`)
+      results = data.tracks.items
+      for (let i = 0; i < 5 && i < results.length; i++) {
+        song = data.tracks.items[i];
+        var resp = `Song: ${song.name}
+        Artist ${song.artists[0].name} 
+        Album: ${song.album.name} 
+        Preview Link: ${song.preview_url}`;
+        output.push(resp);
+        console.log(resp);
+      } 
+      addToLog(`\n
+      ${output[0]}
+      ${output[1]}
+      ${output[2]}
+      ${output[3]}
+      ${output[4]}`);
     }
   });
 };
@@ -75,8 +119,9 @@ function runOMDB(input) {
   var request2 = require('request');
   request2(`http://www.omdbapi.com/?apikey=trilogy&t=${input}`, function (error, response, body) {
     if (error) {
-        console.log(`error: ${error}`);
-        console.log(`statusCode: ${response} && ${response.statusCode}`);
+      output = `OMDB Error: ${error}`, `statusCode: ${response && response.statusCode}`;
+      addToLog(output);
+      console.log(output);
     } else {
       movie = JSON.parse(body);
       if (movie.Ratings.length < 1) {
@@ -86,14 +131,17 @@ function runOMDB(input) {
         imdbRating = movie.Ratings[0].Value;
         rtRating = movie.Ratings[1].Value;
       }
-      console.log(`Title: ${movie.Title}
+      output = `\n
+      Title: ${movie.Title}
       Year: ${movie.Year}
       Country: ${movie.Country}
       Language: ${movie.Language}
       Plot: ${movie.Plot}
       Cast: ${movie.Actors}
       IMDB Rating: ${imdbRating}
-      Rotten Tomatoes Rating: ${rtRating}`);
+      Rotten Tomatoes Rating: ${rtRating}`
+      addToLog(output)
+      console.log(output);
     }
   });
 };
